d6664f68852ec1f2d9c0db56802963df93884a34
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-11-24 15:10:21 +0100

    Create frame_extractor.py

---
+++ frame_extractor.py
@@ -0,0 +1,81 @@
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+import random
+
+
+def select_file():
+    global video_path
+    video_path = filedialog.askopenfilename(title="Seleziona il file video", filetypes=[("Video files", "*.mp4")])
+    file_label.config(text=video_path)
+
+
+def select_folder():
+    global save_path
+    save_path = filedialog.askdirectory(title="Seleziona la cartella di destinazione")
+    folder_label.config(text=save_path)
+
+
+def process_video(num_frames):
+    cap = cv2.VideoCapture(video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    if not num_frames or num_frames > frame_count:
+        num_frames = frame_count
+
+    selected_frames = random.sample(range(1, frame_count + 1), num_frames)
+    selected_frames.sort()
+
+    for i in selected_frames:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i - 1)
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame_name = save_path + "/frame_" + str(i) + ".jpg"
+        cv2.imwrite(frame_name, frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
+    result_label.config(text=f"Estrazione di {num_frames} frame completata!")
+
+
+def extract_frames():
+    num_frames = num_frames_entry.get()
+    if not num_frames.isdigit():
+        num_frames = None
+    else:
+        num_frames = int(num_frames)
+    process_video(num_frames)
+
+
+# Creazione della GUI
+root = tk.Tk()
+root.title("Estrai Frame da Video")
+
+# Pulsanti e etichette
+file_label = tk.Label(root, text="Seleziona il file video:")
+file_label.pack()
+
+file_button = tk.Button(root, text="Scegli File", command=select_file)
+file_button.pack()
+
+folder_label = tk.Label(root, text="Seleziona la cartella di destinazione:")
+folder_label.pack()
+
+folder_button = tk.Button(root, text="Scegli Cartella", command=select_folder)
+folder_button.pack()
+
+num_frames_label = tk.Label(root, text="Numero di frame da estrarre (lascia vuoto per tutti):")
+num_frames_label.pack()
+
+num_frames_entry = tk.Entry(root)
+num_frames_entry.pack()
+
+extract_button = tk.Button(root, text="Estrai Frame", command=extract_frames)
+extract_button.pack()
+
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+root.mainloop()
